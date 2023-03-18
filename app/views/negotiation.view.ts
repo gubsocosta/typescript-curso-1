2666b6c3988c5f34ec07ce603281d7dd593c0d20
Gabriel Costa <gubsocosta@gmail.com>
2023-03-18 18:05:19 -0300

    feat: create negotiation view

---
+++ app/views/negotiation.view.ts
@@ -0,0 +1,17 @@
+export class NegotiatioView {
+  template(): string {
+    return `
+    <table class="table table-hover table-bordered">
+      <thead>
+        <tr>
+          <th>DATE</th>
+          <th>QUANTITY</th>
+          <th>AMOUNT</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+    `
+  }
+}
