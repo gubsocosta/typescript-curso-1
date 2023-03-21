00f8d8047bc3c172df19bf16fcfa0cbe90e34842
Gabriel Costa <gubsocosta@gmail.com>
2023-03-21 19:58:11 -0300

    feat: create util date functions

---
+++ app/utils/date.utils.ts
@@ -0,0 +1,14 @@
+import { DaysOfWeek } from "../enums/days-of-week.enum.js";
+
+export class DateUtils {
+  static convertStringToDate(dateString: string): Date {
+    const regex = /-/g;
+    return new Date(dateString.replace(regex, ","));
+  }
+
+  static isBussinesDay(date: Date): boolean {
+    const noWorkingDays = [DaysOfWeek.SUNDAY, DaysOfWeek.SATURDAY];
+
+    return !noWorkingDays.includes(date.getDay());
+  }
+}
